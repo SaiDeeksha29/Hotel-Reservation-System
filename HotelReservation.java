62f84447465b69f7b932e070268ea661ac09ee1e
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 19:17:15 +0530

    Added:Hotel is added and tested by using a test case

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,7 +1,16 @@
 package com.blz.training.controller;
 
+import java.util.*;
+
 public class HotelReservation {
 
+	private List<Hotel> hotelList = new ArrayList<Hotel>();
+
+	public boolean addHotel(String hotelName, int rateForRegularCustomer) {
+		Hotel hotel = new Hotel(hotelName, rateForRegularCustomer);
+		hotelList.add(hotel);
+		return true;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Hotel Reservation Program in HotelReservation class on Master Branch");
--- HotelReservationTest.java
+++ HotelReservationTest.java
@@ -1,1 +1,16 @@
 package com.blz.training.controller;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class HotelReservationTest {
+
+	@Test
+	public void addHotelForRegularCustomerTest() {
+		HotelReservation hotelReservation = new HotelReservation();
+		boolean checkHotel = hotelReservation.addHotel("Bridgewood", 160);
+		Assert.assertEquals(true, checkHotel);
+	}
+
+}
