778eb323e7191c9913ec0ddd37b0e02a89cdec04
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 19:37:49 +0530

    Added:Rates for regular customer for weekdays and weekends

--- HotelReservation.java
+++ HotelReservation.java
@@ -9,8 +9,8 @@ public class HotelReservation {
 
 	private List<Hotel> hotelList = new ArrayList<Hotel>();
 
-	public boolean addHotel(String hotelName, int rateForRegularCustomer) {
-		Hotel hotel = new Hotel(hotelName, rateForRegularCustomer);
+	public boolean addHotel(String hotelName, int rateForWeekdaysRegularCustomer,int rateForWeekendsRegularCustomer) {
+		Hotel hotel = new Hotel(hotelName, rateForWeekdaysRegularCustomer,rateForWeekendsRegularCustomer);
 		hotelList.add(hotel);
 		return true;
 	}
@@ -26,8 +26,8 @@ public class HotelReservation {
 			e.printStackTrace();
 		}
 		long dateRange=2+(endDate.getTime()-startDate.getTime()) / 1000 / 60 / 60 / 24;
-		Hotel cheapHotel=hotelList.stream().sorted(Comparator.comparing(Hotel::getRateForRegularCustomer)).findFirst().orElse(null);
-		long totalRate=dateRange*cheapHotel.getRateForRegularCustomer();
+		Hotel cheapHotel=hotelList.stream().sorted(Comparator.comparing(Hotel::getRateForWeekdaysRegularCustomer)).findFirst().orElse(null);
+		long totalRate=dateRange*cheapHotel.getRateForWeekdaysRegularCustomer();
 		cheapHotel.setTotalRate(totalRate);
 		return cheapHotel;
 	}
@@ -37,14 +37,16 @@ public class HotelReservation {
 		HotelReservation hotelReservation =new HotelReservation();
 		System.out.println("Welcome to Hotel Reservation Program in HotelReservation class on Master Branch");
         Scanner sc=new Scanner(System.in);
-        hotelReservation.addHotel("Lakewood",110);
-        hotelReservation.addHotel("Bridgewood",160);
-        hotelReservation.addHotel("Ridgewood",220);
+        hotelReservation.addHotel("Lakewood",110,90);
+        hotelReservation.addHotel("Bridgewood",150,50);
+        hotelReservation.addHotel("Ridgewood",220,150);
         System.out.println("Enter the hotel Name of your choice");
         String hotelName=sc.next();
-        System.out.println("Enter the rate for regular customer");
-        int rate=sc.nextInt();
-        hotelReservation.addHotel(hotelName, rate);
+        System.out.println("Enter the rate for Weekdays for regular customer");
+        int rateWeekdays=sc.nextInt();
+        System.out.println("Enter the rate for Weekends for regular customer");
+        int rateWeekends=sc.nextInt();
+        hotelReservation.addHotel(hotelName, rateWeekdays,rateWeekends);
         System.out.println("Enter the start date in ddMMMYYYY format");
         String start=sc.next();
         System.out.println("Enter the end date in ddMMMYYYY format");
