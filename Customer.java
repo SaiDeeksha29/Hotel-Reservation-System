004420bb00271f4d3218e64fd4bee1dadc8f1b75
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-12 21:43:29 +0530

    Modified:Cheapest Best Rated Hotel for Rewards customer

---
+++ Customer.java
@@ -0,0 +1,13 @@
+package com.blz.training.controller;
+
+public class Customer {
+	private String customerType;
+
+	public String getCustomerType() {
+		return customerType;
+	}
+
+	public void setCustomerType(String customerType) {
+		this.customerType = customerType;
+	}
+}
